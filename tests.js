08872e8081f49ea8e294e692cec162af0b037907
TedZamarron <ted.zamarron@gmail.com>
2020-02-10 13:16:29 -0600

    added thrid and fourth test to intro-to-testing

--- tests.js
+++ tests.js
@@ -21,4 +21,10 @@ describe('sayHello', function() {
     it('should return a string when called.', function () {
         expect(typeof sayHello()).toBe("string");
     });
+    it('should return the string "Hello, Jane!" when executed', function () {
+        expect(sayHello()).toBe("Hello, Jane!");
+    });
+    it('should return the string "Hello, Alex! when executed', function () {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
 });
