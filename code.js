d76e03d58850c3f4997a136a5294223ad5bb065a
TedZamarron <ted.zamarron@gmail.com>
2020-02-10 13:45:17 -0600

    added 8th test for intro-to-testing

--- code.js
+++ code.js
@@ -4,5 +4,9 @@ function helloWorld() {
 }
 
 function sayHello(input) {
-    return input === undefined ? "Hello, Jane!" : "Hello, " + input + "!";
+    if(input === undefined || typeof input === "boolean"){
+        return "Hello, World!";
+    } else {
+        return "Hello, " + input + "!";
+    }
 }
--- tests.js
+++ tests.js
@@ -22,12 +22,21 @@ describe('sayHello', function() {
         expect(typeof sayHello()).toBe("string");
     });
     it('should return the string "Hello, Jane!" when executed', function () {
-        expect(sayHello()).toBe("Hello, Jane!");
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
     });
     it('should return the string "Hello, Alex! when executed', function () {
         expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
     it('should return the string "Hello, Pat " when executed', function () {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+    it('should return the string "Hello, World!" when called', function () {
+        expect(sayHello()).toBe("Hello, World!")
+    });
+    it('should return the string "Hello, World! when passed true', function () {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when passed false', function () {
+        expect(sayHello(false)).toBe("Hello, World!");
     });
 });
